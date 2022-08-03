9e909fd800297237a8cafa8958a7d64994309510
Octa <octavio.macieira@aarim.com>
2022-08-03 20:45:26 -0300

    first commit

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            double salario, vendas, salarioFinal;
+
+            nome = Console.ReadLine();
+            salario = double.Parse(Console.ReadLine());
+            vendas = double.Parse(Console.ReadLine());
+
+            salarioFinal = salario + vendas * 15.0/100;
+
+            Console.WriteLine("SALARIO =  R$" + salarioFinal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
